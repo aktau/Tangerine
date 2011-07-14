398adf7e37e7453dd15abf383996c0a9c165a759
Aktau <nicolashillegeer@gmail.com>
2011-07-14 17:11:07 +0200

    Some benchmarks for MatchConflictChecker

--- models/MatchConflictChecker.cc
+++ models/MatchConflictChecker.cc
@@ -9,8 +9,91 @@ using namespace thera;
 
 MatchConflictChecker::MatchConflictChecker(SQLFragmentConf master, QList<SQLFragmentConf> list) : mMaster(master), mList(list) { }
 
+static void compute_contour_overlaps(const thera::Poly2 &src, const thera::Poly2 &tgt, const thera::XF &xf, std::vector<bool> &src_used, std::vector<bool> &tgt_used) {
+	// anything marked as used will be preserved as used
+	assert(src_used.size() == src.size());
+	assert(tgt_used.size() == tgt.size());
+	const float maxsqdist = 1;
+
+	vec2 mn = tgt[0], mx = tgt[0];
+	for (size_t j = 1; j < tgt.size(); j++) {
+		mn = std::min(mn, tgt[j]);
+		mx = std::max(mx, tgt[j]);
+	}
+
+	for (size_t i = 0; i < src.size(); i++) {
+		// assuming planar transform
+		vec2 p(xf[0] * src[i][0] + xf[4] * src[i][1] + xf[12], xf[1]
+				* src[i][0] + xf[5] * src[i][1] + xf[13]);
+
+		if (p[0] < mn[0] - maxsqdist)
+			continue;
+		if (p[0] > mx[0] + maxsqdist)
+			continue;
+		if (p[1] < mn[1] - maxsqdist)
+			continue;
+		if (p[1] > mx[1] + maxsqdist)
+			continue;
+
+		for (size_t j = 0; j < tgt.size(); j++) {
+			if (src_used[i] && tgt_used[j])
+				continue;
+
+			if (dist2(p, tgt[j]) < maxsqdist) {
+				src_used[i] = 1;
+				tgt_used[j] = 1;
+				break;
+			}
+		}
+	}
+}
+
+static void compute_contour_overlaps_stupid(const thera::Poly2 &src, const thera::Poly2 &tgt, const thera::XF &xf, std::vector<bool> &src_used, std::vector<bool> &tgt_used) {
+	// anything marked as used will be preserved as used
+	assert(src_used.size() == src.size());
+	assert(tgt_used.size() == tgt.size());
+	const float maxsqdist = 1;
+
+	vec2 mn = tgt[0], mx = tgt[0];
+	for (size_t j = 1; j < tgt.size(); j++) {
+		mn = std::min(mn, tgt[j]);
+		mx = std::max(mx, tgt[j]);
+	}
+
+	for (size_t i = 0; i < src.size(); i++) {
+		// assuming planar transform
+		vec2 p(xf[0] * src[i][0] + xf[4] * src[i][1] + xf[12], xf[1]
+				* src[i][0] + xf[5] * src[i][1] + xf[13]);
+
+		for (size_t j = 0; j < tgt.size(); j++) {
+			if (src_used[i] && tgt_used[j])
+				continue;
+
+
+			if (p[0] < mn[0] - maxsqdist)
+				continue;
+			if (p[0] > mx[0] + maxsqdist)
+				continue;
+			if (p[1] < mn[1] - maxsqdist)
+				continue;
+			if (p[1] > mx[1] + maxsqdist)
+				continue;
+
+			if (dist2(p, tgt[j]) < maxsqdist) {
+				src_used[i] = 1;
+				tgt_used[j] = 1;
+				break;
+			}
+		}
+	}
+}
+
 QList<SQLFragmentConf> MatchConflictChecker::getConflicting() const {
+	QElapsedTimer t;
+
 	qDebug() << "Checking conflicts for" << mMaster.getTargetId() << "<->" << mMaster.getSourceId() << mMaster.index();
+
+	t.start();
 
     const Fragment *targetFragment = Database::fragment(mMaster.mFragments[IFragmentConf::TARGET]);
     const Fragment *sourceFragment = Database::fragment(mMaster.mFragments[IFragmentConf::SOURCE]);
@@ -21,17 +104,66 @@ QList<SQLFragmentConf> MatchConflictChecker::getConflicting() const {
     targetContour.pin();
     sourceContour.pin();
 
+    qDebug() << "Pinning and retrieving =" << t.restart() << "msec";
+
     //QBitArray targetUsed((*targetContour).size());
     //QBitArray sourceUsed((*sourceContour).size());
 
-    QElapsedTimer t;
-	t.start();
-	for (int k = 0; k < 1000; ++k) {
+    QBitArray qbaTu((*targetContour).size());
+	QBitArray qbaSu((*sourceContour).size());
+	computeOverlap(*sourceContour, *targetContour, mMaster.mXF, qbaSu, qbaTu);
+
+	for (int k = 0; k < 2000; ++k) {
 		QBitArray targetUsed((*targetContour).size());
 		QBitArray sourceUsed((*sourceContour).size());
 		computeOverlap(*sourceContour, *targetContour, mMaster.mXF, sourceUsed, targetUsed);
 	}
-	qDebug() << "Found" << 0 << "overlaps in" << t.elapsed() << "msec";
+	qDebug() << "Found" << 0 << "overlaps in" << t.restart() << "msec";
+
+	std::vector<bool> tgtu;
+	tgtu.resize((*targetContour).size());
+
+	std::vector<bool> srcu;
+	srcu.resize((*sourceContour).size());
+
+	compute_contour_overlaps(*sourceContour, *targetContour, mMaster.mXF, srcu, tgtu);
+
+	for (int k = 0; k < 2000; ++k) {
+	    std::vector<bool> tgt_used;
+	    tgt_used.resize((*targetContour).size());
+
+	    std::vector<bool> src_used;
+	    src_used.resize((*sourceContour).size());
+
+		//computeOverlap(*sourceContour, *targetContour, mMaster.mXF, sourceUsed, targetUsed);
+		compute_contour_overlaps(*sourceContour, *targetContour, mMaster.mXF, src_used, tgt_used);
+	}
+	qDebug() << "std::vector: found" << 0 << "overlaps in" << t.elapsed() << "msec";
+
+	/*
+	for (int k = 0; k < 2000; ++k) {
+		std::vector<bool> tgt_used;
+		tgt_used.resize((*targetContour).size());
+
+		std::vector<bool> src_used;
+		src_used.resize((*sourceContour).size());
+
+		//computeOverlap(*sourceContour, *targetContour, mMaster.mXF, sourceUsed, targetUsed);
+		compute_contour_overlaps_stupid(*sourceContour, *targetContour, mMaster.mXF, src_used, tgt_used);
+	}
+	qDebug() << "std::vector stupid: found" << 0 << "overlaps in" << t.elapsed() << "msec";
+	*/
+
+	assert(qbaSu.size() == srcu.size());
+	assert(qbaTu.size() == tgtu.size());
+	for (int r = 0; r < qbaSu.size(); ++r) {
+		if (qbaSu.testBit(r) != srcu[r]) {
+			qDebug() << "FAIL FAIL FAIL FAIL FAIL";
+			assert(0 == 1);
+		}
+	}
+
+	qDebug() << "Results are the same!!!";
 
     targetContour.unpin();
     sourceContour.unpin();
