9faa685802853221511b1936284db0831e89e583
Aktau <nicolashillegeer@gmail.com>
2011-07-28 13:21:03 +0200

    The humble beginnings of merging code for pure matches

--- dbmerge/MatchMerger.cc
+++ dbmerge/MatchMerger.cc
@@ -11,6 +11,44 @@ MatchMerger::~MatchMerger() {
 }
 
 void MatchMerger::merge(SQLDatabase *left, SQLDatabase *right) {
+	/*
+	for (int i = 0; i < 5; ++i) {
+		QList<SQLFragmentConf> leftMatches = left->getMatches();
+		QList<SQLFragmentConf> rightMatches = right->getMatches();
+	}
+	*/
+
 	QList<SQLFragmentConf> leftMatches = left->getMatches();
 	QList<SQLFragmentConf> rightMatches = right->getMatches();
+
+	typedef QHash<int, const SQLFragmentConf *> IndexMap;
+	IndexMap leftMap;
+	leftMap.reserve(leftMatches.size());
+
+	QElapsedTimer timer;
+	timer.start();
+
+	foreach (const SQLFragmentConf& leftConf, leftMatches) {
+		leftMap.insert(leftConf.index(), &leftConf);
+	}
+
+	qDebug() << "MatchMerger::merge: constructing map of size" << leftMap.size() << "took" << timer.restart() << "msec";
+
+	// if the ID's match, make sure the rest matches as well, if not, re-assign ID
+	IndexMap::const_iterator i, end = leftMap.constEnd();
+	foreach (const SQLFragmentConf& rightConf, rightMatches) {
+		if ((i = leftMap.constFind(rightConf.index())) != end) {
+			qDebug() << "FOUND CONFLICT" << rightConf.index()
+				<< "left:" << i.value()->getSourceId() << "<->" << i.value()->getTargetId()
+				<< "-- right:" << rightConf.getSourceId() << "<->" << rightConf.getTargetId();
+		}
+	}
+
+	/*
+	foreach (const SQLFragmentConf& leftConf, leftMatches) {
+		foreach (const SQLFragmentConf& rightConf, rightMatches) {
+
+		}
+	}
+	*/
 }
